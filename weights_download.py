e13788469326a2efa1b09bc01c77c00c662154d9
kioma <30398225+kioma@users.noreply.github.com>
2017-07-24 05:41:37 +0200

    Update weights_download.py

--- weights_download.py
+++ weights_download.py
@@ -33,7 +33,7 @@ def save_response_content(response, destination):
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
     print(current_dir)
-    file_id = '0Byy2AcGyEVxfUDZwVjU2cFNidTA'
+    file_id = '0Byy2AcGyEVxfVnlCMlBGTDR3RGs'
     destination = current_dir+'/imagenet_weights/densenet161_weights_tf.h5'
 
     download_file_from_google_drive(file_id, destination)
